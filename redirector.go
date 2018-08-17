775f2a48e1a9ba632b180cc15c7c76f92e8904f6
Aofei Sheng <aofei@aofeisheng.com>
2018-08-18 00:04:06 +0800

    refactor: make gases configurable

--- redirector.go
+++ redirector.go
@@ -6,30 +6,44 @@ import (
 	"github.com/sheng/air"
 )
 
-// WWW2NonWWWGas is used to redirect www requests to non-www.
-func WWW2NonWWWGas(next air.Handler) air.Handler {
-	return func(req *air.Request, res *air.Response) error {
-		if strings.HasPrefix(req.URL.Host, "www.") {
-			res.StatusCode = 301
-			u := *req.URL
-			u.Host = u.Host[4:]
-			return res.Redirect(u.String())
-		}
+// WWW2NonWWWGasConfig is a set of configurations for the `WWW2NonWWWGas()`.
+type WWW2NonWWWGasConfig struct {
+}
 
-		return next(req, res)
+// WWW2NonWWWGas returns an `air.Gas` that is used to redirect www requests to
+// non-www.
+func WWW2NonWWWGas(w2nwgc WWW2NonWWWGasConfig) air.Gas {
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) error {
+			if strings.HasPrefix(req.URL.Host, "www.") {
+				res.StatusCode = 301
+				u := *req.URL
+				u.Host = u.Host[4:]
+				return res.Redirect(u.String())
+			}
+
+			return next(req, res)
+		}
 	}
 }
 
-// NonWWW2WWWGas is used to redirect non-www requests to www.
-func NonWWW2WWWGas(next air.Handler) air.Handler {
-	return func(req *air.Request, res *air.Response) error {
-		if !strings.HasPrefix(req.URL.Host, "www.") {
-			res.StatusCode = 301
-			u := *req.URL
-			u.Host = "www." + u.Host
-			return res.Redirect(u.String())
-		}
+// NonWWW2WWWGasConfig is a set of configurations for the `NonWWW2WWWGas()`.
+type NonWWW2WWWGasConfig struct {
+}
 
-		return next(req, res)
+// NonWWW2WWWGas returns an `air.Gas` that is used to redirect non-www requests
+// to www.
+func NonWWW2WWWGas(nw2wgc WWW2NonWWWGasConfig) air.Gas {
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) error {
+			if !strings.HasPrefix(req.URL.Host, "www.") {
+				res.StatusCode = 301
+				u := *req.URL
+				u.Host = "www." + u.Host
+				return res.Redirect(u.String())
+			}
+
+			return next(req, res)
+		}
 	}
 }
