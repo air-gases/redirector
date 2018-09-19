759bf7f404307949981ea444423bc72eef858311
Aofei Sheng <aofei@aofeisheng.com>
2018-09-19 14:38:43 +0800

    refactor: adapt to changes of modules

--- redirector.go
+++ redirector.go
@@ -1,6 +1,7 @@
 package redirector
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/aofei/air"
@@ -15,11 +16,14 @@ type WWW2NonWWWGasConfig struct {
 func WWW2NonWWWGas(w2nwgc WWW2NonWWWGasConfig) air.Gas {
 	return func(next air.Handler) air.Handler {
 		return func(req *air.Request, res *air.Response) error {
-			if strings.HasPrefix(req.URL.Host, "www.") {
-				res.StatusCode = 301
-				u := *req.URL
-				u.Host = u.Host[4:]
-				return res.Redirect(u.String())
+			if strings.HasPrefix(req.Authority, "www.") {
+				res.Status = 301
+				return res.Redirect(fmt.Sprintf(
+					"%s://%s%s",
+					req.Scheme,
+					req.Authority[4:],
+					req.Path,
+				))
 			}
 
 			return next(req, res)
@@ -36,11 +40,14 @@ type NonWWW2WWWGasConfig struct {
 func NonWWW2WWWGas(nw2wgc WWW2NonWWWGasConfig) air.Gas {
 	return func(next air.Handler) air.Handler {
 		return func(req *air.Request, res *air.Response) error {
-			if !strings.HasPrefix(req.URL.Host, "www.") {
-				res.StatusCode = 301
-				u := *req.URL
-				u.Host = "www." + u.Host
-				return res.Redirect(u.String())
+			if !strings.HasPrefix(req.Authority, "www.") {
+				res.Status = 301
+				return res.Redirect(fmt.Sprintf(
+					"%s://www.%s%s",
+					req.Scheme,
+					req.Authority,
+					req.Path,
+				))
 			}
 
 			return next(req, res)
