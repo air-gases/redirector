44669180b518619e6f311fc28bf62a2f73b9bec1
Aofei Sheng <aofei@aofeisheng.com>
2018-10-14 15:55:45 +0800

    feat: enrich redirector.go

--- redirector.go
+++ redirector.go
@@ -7,6 +7,52 @@ import (
 	"github.com/aofei/air"
 )
 
+// HTTP2HTTPSGasConfig is a set of configurations for the `HTTP2HTTPSGas()`.
+type HTTP2HTTPSGasConfig struct {
+}
+
+// HTTP2HTTPSGas returns an `air.Gas` that is used to redirect HTTP requests to
+// HTTPS.
+func HTTP2HTTPSGas(h2hsgc HTTP2HTTPSGasConfig) air.Gas {
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) error {
+			if req.Scheme == "http" {
+				res.Status = 301
+				return res.Redirect(fmt.Sprintf(
+					"https://%s%s",
+					req.Authority,
+					req.Path,
+				))
+			}
+
+			return next(req, res)
+		}
+	}
+}
+
+// HTTPS2HTTPGasConfig is a set of configurations for the `HTTPS2HTTPGas()`.
+type HTTPS2HTTPGasConfig struct {
+}
+
+// HTTPS2HTTPGas returns an `air.Gas` that is used to redirect HTTPS requests
+// to HTTP.
+func HTTPS2HTTPGas(hs2h HTTP2HTTPSGasConfig) air.Gas {
+	return func(next air.Handler) air.Handler {
+		return func(req *air.Request, res *air.Response) error {
+			if req.Scheme == "https" {
+				res.Status = 301
+				return res.Redirect(fmt.Sprintf(
+					"http://%s%s",
+					req.Authority,
+					req.Path,
+				))
+			}
+
+			return next(req, res)
+		}
+	}
+}
+
 // WWW2NonWWWGasConfig is a set of configurations for the `WWW2NonWWWGas()`.
 type WWW2NonWWWGasConfig struct {
 }
